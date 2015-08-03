45758e008cd08cd2ec6915ab211e11c800e34199
MostUnfabulous <Haydenbsmith@gmail.com>
2015-08-03 16:46:09 +0100

    draft One

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$type = $_POST['type'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Email: $email \n Subject: $subject \n Message: $message ";
+$recipient = "info@skyzack.biz";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
